2eebe0f980a0e63c39808b5ee1c516c22c2693ae
farhadfahri <91671369+farhadfahri@users.noreply.github.com>
2024-06-04 23:49:16 +0300

    feat: add types and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
+type Orientation = "N" | "E" | "S" | "W";
+
+interface RoomSize {
+  width: number;
+  height: number;
+}
+
+interface Position {
+  x: number;
+  y: number;
+  orientation: Orientation;
+}
+
 function robotMover() {
-  console.log("start of function added");
+  // get values from cli after verifying logic
+  let x = 5;
+  let y = 5;
+  const commands = "RFLFFLRF";
+
+  const roomSIze: RoomSize = { width: x, height: y };
 }
 
 robotMover();
